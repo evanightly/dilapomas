e65859a214d3596aa773e87c9b7dc97500af0365
evanightly <adamhenderson3x3@gmail.com>
2025-07-16 19:12:27 +0700

    update: add app title in dashboard sidebar

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,9 +1,15 @@
+import { PageProps as InertiaPageProps } from '@inertiajs/core';
 import { AxiosInstance } from 'axios';
 import type { route as routeFn } from 'ziggy-js';
+import { PageProps as AppPageProps } from './';
 declare global {
     interface Window {
         axios: AxiosInstance;
     }
     const axios: AxiosInstance;
     const route: typeof routeFn;
+}
+
+declare module '@inertiajs/core' {
+    interface PageProps extends InertiaPageProps, AppPageProps {}
 }
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -41,3 +41,7 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+    name: string;
+};
